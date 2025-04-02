9841102eb557523a693afc9b5df39ae751a2e006
Margo <romanova184@yandex.ru>
2025-04-02 09:52:38 +0300

    First comit

---
+++ src/js/object.js
@@ -0,0 +1,34 @@
+const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон'
+      }, 
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...'
+        // <- обратите внимание, описание "засекречено"
+      }
+    ]	
+  }
+
+for (const el of character.special) {
+    destructCharacter(el);  
+};
+
+
+export function destructCharacter (specialEl) {
+    let {id, name, icon, description = 'Описание недоступно'} = specialEl;
+    let arrResult = [id, name, description, icon];
+    console.log('Результат: ', arrResult);
+    return arrResult;
+}
